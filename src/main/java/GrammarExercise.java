b614d2d84d1c59be70da0c6af86117df43251f6b
LiYuan1229 <823260355@qq.com>
2020-09-09 19:18:16 +0800

    finish GrammarExercise.java

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,18 +1,45 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 
 public class GrammarExercise {
     public static void main(String[] args) {
-        //需要从命令行读入
-        String firstWordList = "";
-        String secondWordList = "";
+        Scanner input = new Scanner(System.in);
+        String firstWordList = input.nextLine();
+        String secondWordList = input.nextLine();
 
         List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
-        //按要求输出到命令行
+
+        result.stream().forEach(System.out::println);
 
     }
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
-        //在这编写实现代码
-        return null;
+        String[] firstWordArr = firstWordList.toUpperCase().split(",");
+        String[] secondWordArr = secondWordList.toUpperCase().split(",");
+        //验证输入是否合法
+        String regEx = "[A-Z]+";
+        Pattern pattern = Pattern.compile(regEx);
+        for (String str: firstWordArr) {
+            Matcher matcher = pattern.matcher(str);
+            if (!matcher.matches()){
+                throw new RuntimeException("input not valid");
+            }
+        }
+        for (String str: secondWordArr) {
+            Matcher matcher = pattern.matcher(str);
+            if (!matcher.matches()){
+                throw new RuntimeException("input not valid");
+            }
+        }
+        List<String> firstList = Arrays.stream(firstWordArr).distinct().collect(Collectors.toList());
+        List<String> secndList = Arrays.stream(secondWordArr).distinct().collect(Collectors.toList());
+        List<String> result = secndList.stream().filter(firstList::contains).sorted().map(str -> str.replace("", " ").trim()).collect(Collectors.toList());
+
+        return result;
     }
 }
